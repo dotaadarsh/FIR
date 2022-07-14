b32e85d84bb1454ebde587d5b38015ec671f9adc
Aadarsh Kannan <aadarshkannan111@gmail.com>
2022-07-14 03:54:59 +0000

    Minor changes

--- my_app/translate.py
+++ my_app/translate.py
@@ -7,8 +7,10 @@ from googletrans import Translator
 translator = Translator()
 
 st.title('Transliteration')
+
 with st.sidebar:
-    st.text("Connect with me")
+    st.info("Upload the file for which you want to translate - use the sample files 👉")
+
 docx_file = st.file_uploader("Choose a file") 
 
 if docx_file is not None:
@@ -18,32 +20,35 @@ if docx_file is not None:
         st.json(file_details)
 
     if docx_file.type == "text/plain":
-        # Read as string (decode bytes to string)
+        
         raw_text = str(docx_file.read(),"utf-8")
         st.text_area("Extracted Text", raw_text)
         translated_text = translator.translate(raw_text)
-        print(translated_text.text)
+        st.write(translated_text.text)
 
     elif docx_file.type == "application/pdf":
+
             try:
                 with pdfplumber.open(docx_file) as pdf:
                     pages = pdf.pages[0]
                     extract = pages.extract_text()
-                    with st.expander("Extracted Text", expanded=False):
+                    with st.expander("Extracted Text [Click to expand]", expanded=False):
                         st.write(extract)
                     translated_text = translator.translate(extract)
-                    with st.expander("Translated text"):
-                        st.write("Translated text",translated_text.text)
+                    with st.expander("Translated text [Click to expand]"):
+                        st.write(translated_text.text)
 
             except:
                 st.write("None")
 
-    else:   
+    else:
+
         raw_text = docx2txt.process(docx_file)
-        with st.expander("Extracted Text", expanded=False):
-            st.text_area("Extracted Text", raw_text)
+        
+        with st.expander("Extracted Text [Click to expand]", expanded=False):
+            st.text_area(raw_text)
             translated_text = translator.translate(raw_text)
-            print(translated_text.text)
+            st.write(translated_text.text)
         
 with st.expander("Sample files"):
     data = pd.read_csv('/workspace/FIR/Sample-FIR.csv')
